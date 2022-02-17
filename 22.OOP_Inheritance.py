89678d1d9cdb03974515e9bad0e309d41eb5ac83
Mahmut Yaşar EROL <78146026+m-yasarerol@users.noreply.github.com>
2022-02-17 16:49:43 +0300

    Update 22.OOP_Inheritance.py

--- 22.OOP_Inheritance.py
+++ 22.OOP_Inheritance.py
@@ -45,10 +45,54 @@ print(yazilimci1.bilgileri_göster())    # Ad: Ay�e Soyad: Y�ld�z Maas:700
 # ben eğer yazlımcya extra bir özellik eklemek istiyorsam onun init fonk. baştan tanımlamam gerekiyor ve oraya da bildiği dil değişkenini eklemem gerkiyor.
 # önceki aynı değişkenleri yazmama gerek yoktu. super fonksiyonunu kullanabilirdik. mesela müdür sınıfında uygulamalı görelim:
 
-class mudur(calisan):
-    def __init__(self, isim, soyisim, maas,kavramsal_beceri):
-        super().__init__(isim, soyisim, maas)   # gördüğün gibi kalıtım adlığım fonk. değişkenleri toplu olarak tanımladım
-        self.kavramsal_beceri = kavramsal_beceri
-mudur1 = mudur("Yaşar","Erol",8000,"Analitik düşünme")
+# önceki aynı değişkenleri yazmama gerek yoktu. super fonksiyonunu kullanabilirdik. mesela yönetici sınıfında uygulamalı görelim:
 
-print(mudur1.kavramsal_beceri)
+# KALITIM DERS 2
+# şimdi bir de yönetici sınıfı oluşturacağız, o da çalışandan türeceyek, ancak yönetici snıfında da diğerlerinde olmayan başka bir özellik olacak
+# sorumlu oldupu çalışanlar olacak, ve bu yöneticiye çalışan ekleyip silecebileceğiz, kimlerden sorumlu olduğunu listelicez
+class Yonetici(calisan):
+    def __init__(self, isim, soyisim, maas, calisanlar = None):     # önce yöneticiyi oluşturup sonra kimlerden sorumlu olduğunu belirleyeceğim. o nedenle "none" yazdım
+        super().__init__(isim, soyisim, maas)
+        if calisanlar == None:
+            self.calisanlar = []
+        else:
+            self.calisanlar = calisanlar
+        
+    def calisan_ekle(self,calisan):                             # calisan ekleme fonk.
+        if calisan not in self.calisanlar:
+            self.calisanlar.append(calisan)                     # eğer çalışan bu listede yoksa ekle
+
+    def calisan_sil(self,calisan):                              # calisan silme fonk.
+        if calisan in self.calisanlar:
+            self.calisanlar.remove(calisan)                     # eğer çalışan bu listede varsa sil
+
+    def calisanlari_goster(self):                               # sadece self yazdı çünkü görüntülemek istediğim çalışanları ben girmeyeceğim
+        for calisan in self.calisanlar:
+            print(calisan.bilgileri_göster())
+
+yonetici1 = Yonetici("Metin","Ali",10000)
+yonetici1.calisan_ekle(calisan1)
+yonetici1.calisan_ekle(yazilimci1)
+yonetici1.calisanlari_goster()                                  #Ad: Ali Soyad: �al��kan Maas:5000 Email:Ali�al��kan@sirket.com
+                                                                #Ad: Ay�e Soyad: Y�ld�z Maas:7000 Email:Ay�eY�ld�z@sirket.com Bildi�i dil: Phyton
+yonetici1.calisan_sil(calisan1)
+yonetici1.calisanlari_goster()                                  #Ad: Ay�e Soyad: Y�ld�z Maas:7000 Email:Ay�eY�ld�z@sirket.com Bildi�i dil: Phyton
+# yöneticimizin ekleme,silme ve görüntüleme fonk. hepsi düzgün bir şekilde çalışıyor
+
+# bir de çalışanları elle girelim
+yonetici2 = Yonetici("Feyyaz","Beşiktaş",11000,[yazilimci1,yazilimci2,calisan1])
+yonetici2.calisanlari_goster()                                  #Ad: Ay�e Soyad: Y�ld�z Maas:7000 Email:Ay�eY�ld�z@sirket.com Bildi�i dil: Phyton
+                                                                #Ad: Fatma Soyad: Ay Maas:8000 Email:FatmaAy@sirket.com Bildi�i dil: Phyton
+                                                                #Ad: Ali Soyad: �al��kan Maas:5000 Email:Ali�al��kan@sirket.com
+
+# bir sınıf, başka bir sınıfın alt clas' mı veya ondan üretilmiş bir nesne mi nasıl kontrol edeceğiz?
+print(isinstance(yonetici2,calisan))        # True: yani yonetici 2 bir calisandir, yani calisandan üretilmiş bir instance'dir(örnektir)
+print(isinstance(yazilimci1,Yonetici))      # False
+# yani bu isinstance demek, birinci yazdığınız nesne, ikinci yazdığınızdan üretilmiş mi? o anlama geliyor.
+
+#issubclas: bir clas başka bir clasın alt clası mı? ondan türetilmiş mi
+print(issubclass(calisan,Yazilimci))        # false: bu soru şu anlama gelir: çalışan, yazılımcıdan türedi mi?
+print(issubclass(Yazilimci,calisan))        # True: yazılımcı, çalışan clasından türemiş bir clas mıdır?
+
+# bu sorular ile bir class başka bir clasın alt clası mı?
+# bir nesne verdiğimiz bir clastan türemiş mi mi türememiş mi? .. kontrol edebiliyoruz.
